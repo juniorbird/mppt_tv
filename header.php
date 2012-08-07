28c255526669d049e09c17ca163dc5a42476089c
Wade Armstrong <wade@wadearmstrong.com>
2012-08-06 22:52:53 -0700

    #360889 Start of basic themes
    Has:
    * Template appears in themes
    
    Doesn't Have:
    * Template shows ANY entries
    * Template shows pages

---
+++ header.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+</head>
+<body>
+<div id="wrapper" class="hfeed">
+    <div id="header">
+        <div id="masthead">
+         
+            <div id="branding">
+            </div><!-- #branding -->
+             
+            <div id="access">
+            </div><!-- #access -->
+             
+        </div><!-- #masthead -->   
+    </div><!-- #header -->
+     
+    <div id="main">
