15910ee02b764700b8ad2c1ae8ef179872ea1fcc
Wade Armstrong <wade@wadearmstrong.com>
2012-08-07 21:59:04 -0700

    KN #360887: Remove & Reorder menus
    * Re-order menus so that Pages, News, and Team Members are at the top
    -- since that's what editors will use
    * Put dividers in so that those are the obvious ones to use and the
    "dangerous" ones are in other sections
    * Removed the Comments and Links that won't be used

--- functions.php
+++ functions.php
@@ -31,6 +31,9 @@ add_action( 'init', 'change_post_object_label' );
 add_action( 'admin_menu', 'change_post_menu_label' );
 
 
+// Create a new post type for Team Members
+// from http://codex.wordpress.org/Post_Types
+// For Kanbanery #360887
 add_action( 'init', 'create_post_type' );
 function create_post_type() {
 	register_post_type( 'team_member',
@@ -45,4 +48,43 @@ function create_post_type() {
 		)
 	);
 }
+
+
+// Put the menu items in a nice order so that it's obvious for an editor what to do
+// http://wp.tutsplus.com/tutorials/creative-coding/customizing-your-wordpress-admin/
+// For Kanbanery #360887
+
+function custom_menu_order($menu_ord) {
+	if (!$menu_ord) return true;
+	
+	return array(
+		'index.php', // Dashboard
+		'separator1', // First separator
+		'edit.php?post_type=page', // Pages
+		'edit.php', // News
+		'edit.php?post_type=team_member', // Team Members
+		'separator2',
+		'upload.php', // Media
+		'separator-last', // Second separator
+		'themes.php', // Appearance
+		'plugins.php', // Plugins
+		'users.php', // Users
+		'tools.php', // Tools
+		'options-general.php', // Settings
+	);
+}
+add_filter('custom_menu_order', 'custom_menu_order'); // Activate custom_menu_order
+add_filter('menu_order', 'custom_menu_order');
+
+
+// Remove menu items for post types they won't use
+// http://wp.tutsplus.com/tutorials/creative-coding/customizing-your-wordpress-admin/
+// For Kanbanery #360887
+
+function edit_admin_menus() {
+	 remove_menu_page('edit-comments.php'); // Remove the Comments Menu  
+	 remove_menu_page('link-manager.php'); // Remove the Links Menu  
+}
+add_action( 'admin_menu', 'edit_admin_menus' ); 
+
 ?>
