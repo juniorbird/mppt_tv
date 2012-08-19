a2cf3c8005ab8d4f70f81e6c0d21658bb6b742c1
Wade Armstrong <wade@wadearmstrong.com>
2012-08-19 13:41:25 -0700

    KN #375910 Has a home page template
    This can eventually allow me to style the home page?

---
+++ page_home.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the wordpress construct of pages
+ * and that other 'pages' on your wordpress site will use a
+ * different template.
+ *
+ * @package Skeleton WordPress Theme Framework
+ * @subpackage skeleton
+ * @author Simple Themes - www.simplethemes.com
+ */
+
+/**
+ * Template Name: Home Page
+ *
+ * Selectable from a dropdown menu on the edit page screen.
+ */
+
+// You can override via functions.php conditionals or define:
+// $columns = 'four';
+get_header();
+st_before_content($columns='');
+get_template_part( 'loop', 'page' );
+get_footer();
+?>
