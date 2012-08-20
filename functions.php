61df0457088f3f097abcf30ec74a30362f6963ff
Wade Armstrong <wade@wadearmstrong.com>
2012-08-19 19:17:52 -0700

    KN #375910 Styled & News
    Has homepage news copy
    Has a font approximating the one they use

--- functions.php
+++ functions.php
@@ -996,3 +996,30 @@ function edit_admin_menus() {
 	 remove_menu_page('link-manager.php'); // Remove the Links Menu  
 }
 add_action( 'admin_menu', 'edit_admin_menus' ); 
+
+//  Have recent posts shortcode to use on the homepage
+// From http://wp.smashingmagazine.com/2012/05/01/wordpress-shortcodes-complete-guide/
+// For Kanbanery #375910
+function recent_posts_function($atts){
+   extract(shortcode_atts(array(
+      'posts' => 1,
+   ), $atts));
+
+   $return_string = '<ul>';
+   query_posts(array('orderby' => 'date', 'order' => 'DESC' , 'showposts' => $posts));
+   if (have_posts()) :
+      while (have_posts()) : the_post();
+         $return_string .= '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
+      endwhile;
+   endif;
+   $return_string .= '</ul>';
+
+   wp_reset_query();
+   return $return_string;
+}
+
+function register_shortcodes(){
+   add_shortcode('recent-posts', 'recent_posts_function');
+}
+
+add_action( 'init', 'register_shortcodes');
